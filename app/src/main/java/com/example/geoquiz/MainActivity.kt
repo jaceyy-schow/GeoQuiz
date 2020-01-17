a8aacf40eb50947887e3e72745f4cdf0b3317351
jaceyy-schow <jaceyy.schow@gmail.com>
2020-01-17 10:37:09 -0700

    First Commit

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.geoquiz
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+//import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import values.Question
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton:Button
+    private lateinit var falseButton:Button
+    private lateinit var nextButton:Button
+    private lateinit var backButton:Button
+    private lateinit var cheatButton:Button
+    private lateinit var questionTextView: TextView
+
+    private val quizViewModel: QuizViewModel by lazy{
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "OnCreate(Bundle?) called")
+        setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?:0
+        quizViewModel.currentIndex = currentIndex
+
+//        val provider: ViewModelProvider = ViewModelProviders.of(this)
+//        val quizViewModel = provider.get(QuizViewModel::class.java)
+//        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        backButton = findViewById(R.id.back_button)
+        cheatButton = findViewById(R.id.cheat_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+        trueButton.setOnClickListener{view: View ->
+            checkAnswer(true)
+        }
+
+        falseButton.setOnClickListener{view: View->
+            checkAnswer(false)
+        }
+
+        nextButton.setOnClickListener{
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+
+        backButton.setOnClickListener{
+            quizViewModel.moveBack()
+            updateQuestion()
+        }
+        cheatButton.setOnClickListener{
+            //Start CheatActivity
+            //val intent = Intent(this, CheatActivity::class.java)
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            //startActivity(intent)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+        }
+
+        updateQuestion()
+    }
+
+    override fun onActivityResult(requestCode: Int,
+                                  resultCode: Int,
+                                  data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK){
+            return
+        }
+
+        if (requestCode == REQUEST_CODE_CHEAT){
+            quizViewModel.isCheater =
+                data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+    private fun updateQuestion(){
+        //Log.d(TAG, "updating question text", Exception())
+        val questionTextResId = quizViewModel.currentQuestionText
+        questionTextView.setText(questionTextResId)
+    }
+    private fun checkAnswer(userAnswer:Boolean) {
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgement_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+}
